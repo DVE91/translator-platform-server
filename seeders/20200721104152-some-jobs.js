ffff53ff9754ec90ed929f7f5d97710cc3fbd70b
DVE91 <dianevaneldik@gmail.com>
2020-07-21 13:30:19 +0200

    added seeders for profiles, jobs, languages and profileLanguages

---
+++ seeders/20200721104152-some-jobs.js
@@ -0,0 +1,31 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "jobs",
+      [
+        {
+          profileId: 1,
+          title: "5 leukste steden in Noord-Italië",
+          type: "blogpost",
+          wordCount: 820,
+          originalLanguage: "Dutch",
+          nativeLanguage: "Engels (UK)",
+          originalDocument: "Ontdek Verona, Piemonte, Friuli en meer!",
+          translatedDocument: "Discover Verona, Piemonte, Friuli and more!",
+          submitted: false,
+          startingDate: new Date(2020, 8, 15),
+          endDate: new Date(2020, 8, 16),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("jobs", null, {});
+  },
+};
